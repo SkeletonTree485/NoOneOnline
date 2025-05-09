c4f94b5cdfce87ae292e2a643832a4562b6a2cd4
SkeletonTree485 <136203447+SkeletonTree485@users.noreply.github.com>
2025-05-10 00:35:36 +0200

    shorter interval

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function changeBackground() {
 }
 
 // Change background every 5 seconds
-setInterval(changeBackground, 15000);
+setInterval(changeBackground, 1000);
 
 // Initial background change
 changeBackground();
